06bb5cfb8d6e608c9806711a4760435a354f049f
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2024-11-12 01:22:49 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let fname = document.getElementById("fName");
+        let lname = document.getElementById("lName");
+        let pnumber = document.getElementById("pNumber");
+        let email = document.getElementById("eMail");
+
+        function Submit(){
+            alert(`First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${pnumber.value} Email ID: ${email.value}`);
+        }
